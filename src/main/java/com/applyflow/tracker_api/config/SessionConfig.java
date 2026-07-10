c45e5c83e31e48ea950940176a248b1137991d8f
jecem ben slama <benslemajecem@gmail.com>
2026-07-10 03:29:29 +0100

    feat: add SessionConfig for cookie serialization and session management

---
+++ src/main/java/com/applyflow/tracker_api/config/SessionConfig.java
@@ -0,0 +1,20 @@
+package com.applyflow.tracker_api.config;
+
+import org.springframework.session.web.http.DefaultCookieSerializer;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SessionConfig {
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("SESSION");
+        serializer.setUseSecureCookie(true);
+        serializer.setSameSite("None");
+        serializer.setCookiePath("/");
+        return serializer;
+    }
+}
