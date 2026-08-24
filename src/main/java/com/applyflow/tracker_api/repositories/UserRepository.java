58d93997c7adc8ab2026f4f469ad8196ed0ccbd6
jecem ben slama <benslemajecem@gmail.com>
2026-08-24 01:42:32 +0100

    remove caching

--- src/main/java/com/applyflow/tracker_api/repositories/UserRepository.java
+++ src/main/java/com/applyflow/tracker_api/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.applyflow.tracker_api.repositories;
 
 import com.applyflow.tracker_api.models.User;
-import org.springframework.cache.annotation.Cacheable; // 👈 Add this import
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // Cache user objects by their ID. Subsequent calls return instantly without
     // SQL.
-    @Cacheable(value = "users", key = "#id")
     Optional<User> findById(Long id);
 
     Optional<User> findByGoogleSub(String googleSub);
